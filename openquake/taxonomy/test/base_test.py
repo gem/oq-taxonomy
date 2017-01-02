250f985a0cd41bb05d41af332eb998fd497c1c42
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-02 10:20:12 +0100

    increase timeout wait_new_page

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -98,7 +98,7 @@ class TaxonomyAllTest(unittest.TestCase):
         termlink.click()
 
         pla.wait_new_page(termlink, 'terms/height-of-ground-floor-level-above'
-            '-grade--hf')
+            '-grade--hf', timeout=10)
 
         img = pla.xpath_finduniq(
             "//img[@alt='HF_diagram_-_1']")
