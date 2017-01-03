143b55ad7cb283effa01f63d4afa942062c268eb
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-03 14:14:54 +0100

    modify test alltaxonomy  base_test.py

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -37,14 +37,14 @@ class TaxonomyInOutTest(unittest.TestCase):
 
         #pla.wait_new_page(submittermlink, 'index.php/submit-an-article')
 
-        # insert_title_field = pla.xpath_finduniq(
-        #     "//input[@id='jform_title' and @type='text' and"
-        #     " @name='jform[title]']")
-        # insert_title_field.send_keys(exex)
+        insert_title_field = pla.xpath_finduniq(
+            "//input[@id='jform_title' and @type='text' and"
+            " @name='jform[title]']")
+        insert_title_field.send_keys(exex)
 
-        # submit_button_insert = pla.xpath_finduniq(
-        #     "//button[@type='button' and text()='Save']")
-        # submit_button_insert.click()
+        submit_button_insert = pla.xpath_finduniq(
+            "//button[@type='button' and text()='Save']")
+        submit_button_insert.click()
 
     def tearDown(self):
         submit_logout = pla.xpath_finduniq(
