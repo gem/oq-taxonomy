0d352f1580858b038a1e1ababc8dfb99c6dfb343
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-03 14:25:33 +0100

    delete method setup and teardown

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -26,7 +26,7 @@ class TaxonomyInOutTest(unittest.TestCase):
         submit_login.click()
 
 
-    def insert_test(self):
+    # def insert_test(self):
         # pla.get('')
 
         exex = 'term example'
@@ -35,7 +35,7 @@ class TaxonomyInOutTest(unittest.TestCase):
             "//a[normalize-space(text())='Submit new term']")
         submit_termlink.click()
 
-        #pla.wait_new_page(submittermlink, 'index.php/submit-an-article')
+        pla.wait_new_page(submit_termlink, 'index.php/submit-an-article')
 
         insert_title_field = pla.xpath_finduniq(
             "//input[@id='jform_title' and @type='text' and"
@@ -46,7 +46,7 @@ class TaxonomyInOutTest(unittest.TestCase):
             "//button[@type='button' and text()='Save']")
         submit_button_insert.click()
 
-    def tearDown(self):
+    # def tearDown(self):
         submit_logout = pla.xpath_finduniq(
             "//input[@type='submit' and @name='Submit' and"
             " @value='Log out']")
