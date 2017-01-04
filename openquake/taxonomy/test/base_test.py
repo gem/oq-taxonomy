b1a62339f58e825109e9cadaf2c5938436953399
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-04 16:12:25 +0100

    modify methods setup and teardown

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -48,6 +48,8 @@ class TaxonomyInOutTest(unittest.TestCase):
             " @name='jform[title]']")
         insert_title_field.send_keys(exex)
 
+        time.sleep(self.tout) 
+
         submit_button_insert = pla.xpath_finduniq(
             "//button[@type='button' and text()='Save']")
         submit_button_insert.click()
