cdc73f1e5a6510e89564f791ac7d81bfbf895702
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-09 17:23:09 +0100

    create new method for tests teardown and setup

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -63,10 +63,10 @@ class TaxonomyInOutTest(unittest.TestCase):
         
         submit_termlink = pla.xpath_finduniq(
             "//a[@href='/index.php/submit-an-article' and"
-            " normalize-space(text())='Submit new term']")
+            " normalize-space(text())='Submit new term']", timeout=50)
         submit_termlink.click()
         
-        # pla.wait_new_page(submit_termlink, 'index.php/submit-an-article', timeout=100)
+        # pla.wait_new_page(submit_termlink, 'index.php/submit-an-article')
         
         insert_title_field = pla.xpath_finduniq(
             "//input[@id='jform_title' and @type='text' and"
