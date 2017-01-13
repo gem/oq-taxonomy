e4fe0224d0919a3ae05feb1c0372b7c00fdf3fec
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-13 16:45:01 +0100

    adjust verifier

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -42,22 +42,20 @@ echo "create database gloss" | mysql -u root --password=PASSWORD
 mysql -u root --password=PASSWORD gloss < /tmp/gloss.sql
 
 #copy folder $GEM_GIT_PACKAGE from home lxc to /var/www/html
-sudo cp -R $GEM_GIT_PACKAGE/* $GEM_GIT_PACKAGE/.htaccess /var/www/html
+# sudo cp -R $GEM_GIT_PACKAGE/* $GEM_GIT_PACKAGE/.htaccess /var/www/html
 cd /var/www/html
 sudo wget https://github.com/joomla/joomla-cms/releases/download/3.6.5/Joomla_3.6.5-Stable-Full_Package.zip
 sudo apt-get install unzip
-sudo unzip Joomla_3.6.5-Stable-Full_Package.zip -d /var/www/html/
-sudo cp -R /home/ubuntu/oq-taxonomy/templates/protostar/index.php /var/www/html/templates/protostar/index.php
-sudo cp -R /home/ubuntu/oq-taxonomy/templates/protostar/css/template.css /var/www/html/templates/protostar/css/template.css
-sudo cp -R /home/ubuntu/oq-taxonomy/components/com_finder/views/search/tmpl/default.php /var/www/html/components/com_finder/views/search/tmpl/default.php
-sudo cp -R /home/ubuntu/oq-taxonomy/components/com_content/views/article/tmpl/default.php /var/www/html/components/com_content/views/article/tmpl/default.php
-sudo cp -R /home/ubuntu/oq-taxonomy/administrator/templates/isis/css/template.css /var/www/html/administrator/templates/isis/css/template.css
-sudo cp -R /home/ubuntu/oq-taxonomy/administrator/templates/isis/images/joomla.png /var/www/html/administrator/templates/isis/images/joomla.png
-sudo cp -R /home/ubuntu/oq-taxonomy/configuration.php /var/www/html/configuration.php
-sudo cp -R /home/ubuntu/oq-taxonomy/glossary-term /var/www/html/glossary-term
-sudo cp -R /home/ubuntu/oq-taxonomy/images/headers /var/www/html/images/headers
-sleep 40000
-
+sudo unzip Joomla_3.6.5-Stable-Full_Package.zip
+sudo cp -R /home/ubuntu/oq-taxonomy/templates/protostar/index.php /var/www/html/templates/protostar/
+sudo cp -R /home/ubuntu/oq-taxonomy/templates/protostar/css/template.css /var/www/html/templates/protostar/css/
+sudo cp -R /home/ubuntu/oq-taxonomy/components/com_finder/views/search/tmpl/default.php /var/www/html/components/com_finder/views/search/tmpl/
+sudo cp -R /home/ubuntu/oq-taxonomy/components/com_content/views/article/tmpl/default.php /var/www/html/components/com_content/views/article/tmpl/
+sudo cp -R /home/ubuntu/oq-taxonomy/administrator/templates/isis/css/template.css /var/www/html/administrator/templates/isis/css/
+sudo cp -R /home/ubuntu/oq-taxonomy/administrator/templates/isis/images/joomla.png /var/www/html/administrator/templates/isis/images/
+sudo cp -R /home/ubuntu/oq-taxonomy/configuration.php /var/www/html/
+sudo cp -R /home/ubuntu/oq-taxonomy/glossary-term /var/www/html/
+sudo cp -R /home/ubuntu/oq-taxonomy/images/headers /var/www/html/images/
 
 #set permissions /var/www/html
 sudo chown -R www-data.www-data /var/www/html
@@ -79,3 +77,4 @@ export DISPLAY=:1
 export PYTHONPATH=oq-moon:$GEM_GIT_PACKAGE:$GEM_GIT_PACKAGE/openquake/taxonomy/test/config
 python -m openquake.moon.nose_runner --failurecatcher prod -s -v --with-xunit --xunit-file=xunit-platform-prod.xml $GEM_GIT_PACKAGE/openquake/taxonomy/test || true
 # sleep 40000 || true
+sleep 40000
