8d6071d53fab80e096dc32c09a1be7479e85ed39
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-17 16:58:01 +0100

    changes style

--- components/com_finder/views/search/tmpl/default.php
+++ components/com_finder/views/search/tmpl/default.php
@@ -64,7 +64,7 @@ if ($this->query->search === true):
 <!--<span class="icon-arrow-right arr" style=""></span>
 	<span class="icon-arrow-right arr" style=""></span>-->
 	<?php foreach($results as $rows){ ?>
-		<div class="div-let-cat" <?php printf("%s%s", $rows->cat, ($namee == $rows->cat ? ' let-selected' : '')); ?>>
+		<div class="div-let-cat <?php printf("%s%s", $rows->cat, ($namee == $rows->cat ? ' let-selected' : '')); ?>">
 		<a class="let-cat" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $rows->cat; ?>" style="padding:4px;">
 			<?php echo ucfirst($rows->cat); ?>
 		</a>
