568e9bce846f2ae03a5e8869b34d061549b44d26
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-19 10:45:01 +0100

    changes parameters

--- components/com_finder/views/search/tmpl/default.php
+++ components/com_finder/views/search/tmpl/default.php
@@ -64,14 +64,14 @@ if ($this->query->search === true):
 <!--<span class="icon-arrow-right arr" style=""></span>
 	<span class="icon-arrow-right arr" style=""></span>-->
 	<?php foreach($results as $rows){ ?>
-        <a class="let-cat" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $rows->cat; ?>">
-		<div class="div-let-cat <?php printf("%s%s", $rows->cat, ($namee == $rows->cat ? ' let-selected' : '')); ?>">
+        <a class="let-cat <?php printf("%s%s", $rows->cat, ($namee == $rows->cat ? ' let-selected' : '')); ?>" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $rows->cat; ?>">
+		<div class="div-let-cat">
 			<?php echo ucfirst($rows->cat); ?>
 		</div>
         </a>
 	<?php } ?>
 	<?php foreach($results_count as $rows_count){ ?>
-			<a class="let-cat <?php echo $rows_count->numb; ?>" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $rows_count->numb; ?>" style="    padding:4px;">
+			<a class="let-cat <?php printf("%s%s", $rows->cat, ($namee == $rows->cat ? ' let-selected' : '')); ?>" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $rows_count->numb; ?>">
             <div class="div-let-cat">
 				<?php echo ucfirst($rows_count->numb); ?>
 			</div>
