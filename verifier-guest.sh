36189c85e1a8323b2ca269e71f52a57088e4981f
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-19 17:02:48 +0100

    add command for generated new string alfanumeric

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -58,9 +58,9 @@ sudo cp -Rf $HOME/oq-taxonomy/templates/protostar/index.php /var/www/html/templa
 
 #rename conf and insert variable used
 if [ -f configuration.php.tmpl ] ; then
-sudo cp -Rf $HOME/oq-taxonomy/configuration.php.tmpl /var/www/html/configuration.php
-NEW_UUID_MORE_CHARACTERS=$(cat /dev/urandom | tr -dc "[:alnum:]" | fold -w 16 | head -n 1)
-sudo sed -i 's/5yVmnN9r8jXgbfsl/'$NEW_UUID_MORE_CHARACTERS'/g' /var/www/html/configuration.php
+    sudo cp -Rf $HOME/oq-taxonomy/configuration.php.tmpl /var/www/html/configuration.php
+    NEW_UUID_MORE_CHARACTERS=$(cat /dev/urandom | tr -dc "[:alnum:]" | fold -w 16 | head -n 1)
+    sudo sed -i 's/5yVmnN9r8jXgbfsl/'$NEW_UUID_MORE_CHARACTERS'/g' /var/www/html/configuration.php
 fi
 
 #delete setup installation and zip downloaded
