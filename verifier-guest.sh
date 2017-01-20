554cd6f8623ca925825986bab5e179abd858b69a
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-01-20 14:10:52 +0100

    changes indentation and other comments

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-branch_id="$1"
+BRANCH_ID="$1"
+NO_EXEC_TEST="$3"
+
 #display each command before executing it
 set -x
 . .gem_init.sh
@@ -8,7 +10,7 @@ set -x
 #apt-get update/upgrade
 sudo apt-get -y update
 sudo apt-get -y upgrade
- 
+
 #install apache and addictions php
 sudo apt-get -y install apache2 libapache2-mod-php7.0 php7.0-mysql php7.0-gd php7.0-mcrypt php7.0-mbstring php7.0-zip php7.0-xml
 
@@ -45,8 +47,11 @@ mysql -u root --password=PASSWORD gloss < /tmp/gloss.sql
 #copy folder $GEM_GIT_PACKAGE from home lxc to /var/www/html
 sudo cp -R $GEM_GIT_PACKAGE/* $GEM_GIT_PACKAGE/.htaccess /var/www/html
 
+#
 #for help on this procedure visit https://help.ubuntu.com/community/Joomla
+#
 #download and unzip new version cms for official repo 
+#
 NUM_VER="3.6.5"
 cd /var/www/html
 sudo wget https://github.com/joomla/joomla-cms/releases/download/${NUM_VER}/Joomla_${NUM_VER}-Stable-Full_Package.zip
@@ -75,9 +80,7 @@ sudo rm /var/www/html/index.html
 # sleep 40000
 cd ~
 
-# used variable for deactivated begin tests
-NO_EXEC_TEST="$3"
-
+#function complete procedure for tests
 exec_test () {    
     #install selenium,pip,geckodriver,clone oq-moon and execute tests with nose 
     sudo apt-get -y install python-pip
@@ -87,7 +90,7 @@ exec_test () {
     wget http://ftp.openquake.org/mirror/mozilla/geckodriver-latest-linux64.tar.gz ; tar zxvf geckodriver-latest-linux64.tar.gz ; sudo cp geckodriver /usr/local/bin
 
     cp $GEM_GIT_PACKAGE/openquake/taxonomy/test/config/moon_config.py.tmpl $GEM_GIT_PACKAGE/openquake/taxonomy/test/config/moon_config.py
-    git clone -b "$branch_id" --depth=1  $GEM_GIT_REPO/oq-moon.git || git clone --depth=1 $GEM_GIT_REPO/oq-moon.git
+    git clone -b "$BRANCH_ID" --depth=1  $GEM_GIT_REPO/oq-moon.git || git clone --depth=1 $GEM_GIT_REPO/oq-moon.git
 
     export DISPLAY=:1
     export PYTHONPATH=oq-moon:$GEM_GIT_PACKAGE:$GEM_GIT_PACKAGE/openquake/taxonomy/test/config
