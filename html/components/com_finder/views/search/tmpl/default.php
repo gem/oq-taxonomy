c573e61c07a5f2160254d80844444e4f43f5bdb1
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-02-10 11:24:10 +0100

    changed query escape and deleted table not used on db sql

--- html/components/com_finder/views/search/tmpl/default.php
+++ html/components/com_finder/views/search/tmpl/default.php
@@ -59,9 +59,8 @@ if ($this->query->search === true):
        foreach($results as $rows) {
            //control if exist terms with specific cat
            $cat = $rows->cat;
-           //$sql_term_id = sprintf("SELECT * FROM `taxonomy_content` where title like '%s%%' and catid = '8' and state = '1' ",
-           //                        $db->quote($cat));
-           $sql_term_id = "SELECT * FROM `taxonomy_content` where title like '$cat%' and catid = '8' and state = '1' ";
+           $sql_term_id = sprintf("SELECT * FROM `taxonomy_content` where title like '%s%%' and catid = '8' and state = '1' ",
+                                   $db->escape($cat));
            $db->setQuery($sql_term_id);
            $count_results_term = $db->loadObjectList();
            $db->query($sql_term_id);
