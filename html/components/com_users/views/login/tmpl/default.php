32c8ff3f22c060dc060f27f0aeeef9cde2496628
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-02 15:19:28 +0100

    added index for components users login

---
+++ html/components/com_users/views/login/tmpl/default.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$db =& JFactory::getDBO();
+$sql = "SELECT * FROM `category` ORDER BY id ASC ";
+$db->setQuery($sql);
+$results = $db->loadObjectList();
+?>
+
+
+<div class="term-let">
+                <?php foreach($results as $rows){
+            //control if exist terms with specific cat
+            $cat = $rows->cat;
+            $sql_term_id = sprintf("SELECT * FROM `taxonomy_content` where title like '%s%%' and catid = '8' and state = '1' ",
+                                   $db->escape($cat));
+            $db->setQuery($sql_term_id);
+            $count_results_term = $db->loadObjectList();
+            $db->query($sql_term_id);
+            $count_results = $db->getNumRows();
+            if($count_results == '0'){
+                echo '<style>.m'.$cat.'{display:none;}</style>';
+            }
+        ?>
+            <a class="let-cat m<?php echo $cat; ?>" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $cat; ?>">
+                        <div class="div-let-cat">
+                                <?php echo ucfirst($cat); ?>
+                        </div>
+            </a>
+                <?php } ?>
+                </div>
+<div style="clear:both;"></div>
+
+
+
+<?php
+$cookieLogin = $this->user->get('cookieLogin');
+
+if ($this->user->get('guest') || !empty($cookieLogin))
+{
+        // The user is not logged in or needs to provide a password.
+        echo $this->loadTemplate('login');
+}
+else
+{
+        // The user is already logged in.
+        echo $this->loadTemplate('logout');
+}
