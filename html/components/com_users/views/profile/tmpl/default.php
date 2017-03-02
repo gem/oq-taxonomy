82b3db825cce599bab02ccb7c9ff231254485a4c
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-02 15:34:13 +0100

    added index for components users profile

---
+++ html/components/com_users/views/profile/tmpl/default.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$db =& JFactory::getDBO();
+$sql = "SELECT * FROM `category` ORDER BY id ASC ";
+$db->setQuery($sql);
+$results = $db->loadObjectList();
+?>
+
+<div class="term-let">
+                <?php foreach($results as $rows){
+            //control if exist terms with specific cat
+            $cat = $rows->cat;
+            $sql_term_id = sprintf("SELECT * FROM `taxonomy_content` where title like '%s%%' and catid = '8' and state = '1' ",
+                                   $db->escape($cat));
+            $db->setQuery($sql_term_id);
+            $count_results_term = $db->loadObjectList();
+            $db->query($sql_term_id);
+            $count_results = $db->getNumRows();
+            if($count_results == '0'){
+                echo '<style>.m'.$cat.'{display:none;}</style>';
+            }
+        ?>
+            <a class="let-cat m<?php echo $cat; ?>" href="<?php echo $this->baseurl; ?>/?cat=<?php echo $cat; ?>">
+                        <div class="div-let-cat">
+                                <?php echo ucfirst($cat); ?>
+                        </div>
+            </a>
+                <?php } ?>
+                </div>
+<div style="clear:both;"></div>
+
+
+
+<div class="profile<?php echo $this->pageclass_sfx?>">
+<?php if ($this->params->get('show_page_heading')) : ?>
+<div class="page-header">
+        <h1>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+        </h1>
+</div>
+<?php endif; ?>
+<?php if (JFactory::getUser()->id == $this->data->id) : ?>
+<ul class="btn-toolbar pull-right">
+        <li class="btn-group">
+                <a class="btn" href="<?php echo JRoute::_('index.php?option=com_users&task=profile.edit&user_id=' . (int) $this->data->id);?>">
+                        <span class="icon-user"></span> <?php echo JText::_('COM_USERS_EDIT_PROFILE'); ?></a>
+        </li>
+</ul>
+<?php endif; ?>
+<?php echo $this->loadTemplate('core'); ?>
+
+<?php echo $this->loadTemplate('params'); ?>
+
+<?php echo $this->loadTemplate('custom'); ?>
+
+</div>
