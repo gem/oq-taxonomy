adccc6a99f7f1fe9ab1441fd2531b7f445aeede8
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-03 14:02:54 +0100

    added new test for link top

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -6,7 +6,8 @@ from openquake.taxonomy.test import pla
 
 
 class TaxonomyAllTest(unittest.TestCase):
-    def linkTopTest(self):
+
+    def linktop_test(self):
 
         pla.get()
 
@@ -53,22 +54,19 @@ class TaxonomyAllTest(unittest.TestCase):
 
     def search_test(self):
         
-        pla.get('')
+        pla.get('http://dev-asc.openquake.org/index.php/component/search/')
 
         varsearch = 'Assembly [ASS]'
 
         search_field = pla.xpath_finduniq(
-            "//input[@id='mod-search-searchword' and @type='search']")
+            "//input[@id='mod-search-searchword' and @type='text']")
         search_field.send_keys(varsearch)
 
-        # pla.wait_new_page(search_field, '/index.php/component/search/'
-        #                      '?searchword=Assembly%20[ASS]&'
-        #                      'searchphrase=all&Itemid=101', timeout=10)
-        searchlink = pla.xpath_finduniq(
-            "//a[normalize-space(text())='Assembly [ASS]']")
-        searchlink.click()
+        # searchlink = pla.xpath_finduniq(
+        #     "//a[normalize-space(text())='Assembly [ASS]']")
+        # searchlink.click()
 
-        pla.wait_new_page(searchlink, 'terms/assembly--ass', timeout=5)
+        # pla.wait_new_page(searchlink, 'terms/assembly--ass', timeout=5)
 
  
     def content_test(self):
