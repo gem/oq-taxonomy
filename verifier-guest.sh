c1625a1c9914ba4a86618b19f09d2c2897c94aa1
Matteo Nastasi <nastasi@openquake.org>
2017-08-23 11:00:31 +0200

    externalized and centralized version dependencies for selenium and geckodriver

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -86,9 +86,11 @@ exec_test () {
     sudo apt-get -y install python-pip
     sudo pip install --upgrade pip
     sudo pip install nose
-    sudo pip install -U selenium==3.5.0
-    # wget http://ftp.openquake.org/mirror/mozilla/geckodriver-latest-linux64.tar.gz ; tar zxvf geckodriver-latest-linux64.tar.gz ; sudo cp geckodriver /usr/local/bin
-    wget http://ftp.openquake.org/mirror/mozilla/geckodriver-v0.18.0-linux64.tar.gz ; tar zxvf geckodriver-v0.18.0-linux64.tar.gz ; sudo cp geckodriver /usr/local/bin
+    wget "http://ftp.openquake.org/common/selenium-deps"
+    GEM_FIREFOX_VERSION="$(dpkg-query --show -f '${Version}' firefox)"
+    . selenium-deps
+    sudo pip install -U selenium==${GEM_SELENIUM_VERSION}
+    wget http://ftp.openquake.org/mirror/mozilla/geckodriver-v${GEM_GECKODRIVER_VERSION}-linux64.tar.gz ; tar zxvf geckodriver-v${GEM_GECKODRIVER_VERSION}-linux64.tar.gz ; sudo cp geckodriver /usr/local/bin
 
     cp $GEM_GIT_PACKAGE/openquake/taxonomy/test/config/moon_config.py.tmpl $GEM_GIT_PACKAGE/openquake/taxonomy/test/config/moon_config.py
     git clone -b "$BRANCH_ID" --depth=1  $GEM_GIT_REPO/oq-moon.git || git clone --depth=1 $GEM_GIT_REPO/oq-moon.git
