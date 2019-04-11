344ff5d4b1e83f1e63224e304d933ac220edb670
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-04-11 15:21:07 +0200

    refreshed user component with new version

--- html/components/com_users/views/login/tmpl/default.php
+++ html/components/com_users/views/login/tmpl/default.php
@@ -3,11 +3,24 @@
  * @package     Joomla.Site
  * @subpackage  com_users
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
+
+$cookieLogin = $this->user->get('cookieLogin');
+
+if (!empty($cookieLogin) || $this->user->get('guest'))
+{
+	// The user is not logged in or needs to provide a password.
+	echo $this->loadTemplate('login');
+}
+else
+{
+	// The user is already logged in.
+	echo $this->loadTemplate('logout');
+}
 
 $db =& JFactory::getDBO();
 $sql = "SELECT * FROM `category` ORDER BY id ASC ";
@@ -37,19 +50,3 @@ $results = $db->loadObjectList();
         ?>         
        </div>
        <div style="clear:both;"></div>
-
-
-
-<?php
-$cookieLogin = $this->user->get('cookieLogin');
-
-if ($this->user->get('guest') || !empty($cookieLogin))
-{
-        // The user is not logged in or needs to provide a password.
-        echo $this->loadTemplate('login');
-}
-else
-{
-        // The user is already logged in.
-        echo $this->loadTemplate('logout');
-}
