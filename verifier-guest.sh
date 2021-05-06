da4ce50015b01ba399c2d2f8b9f33b309a546ecf
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2021-05-06 16:25:15 +0200

    add -y to apt-get

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -27,7 +27,7 @@ inst_docker () {
     "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
     # install stable release of docker-compose
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
