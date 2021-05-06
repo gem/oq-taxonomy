51ff043037ba44256f7037a66df5d818d760225d
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2021-05-06 16:58:32 +0200

    add glossary to group docker

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -33,6 +33,8 @@ inst_docker () {
     # install stable release of docker-compose
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    # Add user glossary to group docker
+    usermod -G docker glossary
 }
 
 #installation of docker and docker-compose
