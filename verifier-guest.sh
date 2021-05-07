3b1dc2c6e0005175b78287f37269e6681deb5ae8
armando.scarpati <armando.scarpati@globalquakemodel.org>
2021-05-07 19:18:36 +0200

    moved chown after configuration php management

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -42,8 +42,6 @@ CURRENT_UID=$(id -u):$(id -g) docker-compose up -d db
 
 sleep 20
 
-sudo chown -R glossary:glossary $HOME/$GEM_GIT_PACKAGE/site
-
 CURRENT_UID=$(id -u):$(id -g) docker-compose down
 
 #copy folder $GEM_GIT_PACKAGE from home lxc to /var/www/html
@@ -70,6 +68,8 @@ rm -rf $HOME/$GEM_GIT_PACKAGE/site/images/headers
 sleep 5
 
 CURRENT_UID=$(id -u):$(id -g) docker-compose up -d
+
+sudo chown -R glossary:glossary $HOME/$GEM_GIT_PACKAGE/site
 
 # need to add check to mysql UP
 CURRENT_UID=$(id -u):$(id -g) docker-compose exec -T db mysql -u root --password="PASSWORD" taxonomy < ./taxonomy.sql
