5b8bd437a161fb6ce552d82e052890b4f2b433ba
Matteo Nastasi <nastasi@openquake.org>
2022-01-13 09:48:44 +0100

    add retrieve of ssh key from github.com

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -82,6 +82,11 @@ exec_test () {
     sudo pip install -U selenium==${GEM_SELENIUM_VERSION}
 
     cp $HOME/$GEM_GIT_PACKAGE/openquake/taxonomy/test/config/moon_config.py.tmpl $HOME/$GEM_GIT_PACKAGE/openquake/taxonomy/test/config/moon_config.py
+    github_key="$(ssh-keyscan -t rsa github.com)"
+    if ! grep -q "$github_key" $HOME/.ssh/known_hosts; then
+        echo "$github_key" >> $HOME/.ssh/known_hosts
+    fi
+    
     git clone -b "$BRANCH_ID" --depth=1  $GEM_GIT_REPO/oq-moon.git || git clone --depth=1 $GEM_GIT_REPO/oq-moon.git
     export DISPLAY=:1
     export PYTHONPATH=oq-moon:$HOME/$GEM_GIT_PACKAGE:$HOME/$GEM_GIT_PACKAGE/openquake/taxonomy/test/config
