12afb37b02cadc46adbbf2b6b94c76b43fbf8fea
hascar <armando.scarpati@globalquakemodel.org>
2023-03-09 17:16:26 +0100

    added taxonomy_to_import.sql

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -64,7 +64,7 @@ cp $HOME/$GEM_GIT_PACKAGE/configuration.php.tmpl $HOME/$GEM_GIT_PACKAGE/site/con
 cp -R $HOME/$GEM_GIT_PACKAGE/html/.htaccess $HOME/$GEM_GIT_PACKAGE/site
 # 
 #import mysql db
-CURRENT_UID=$(id -u):$(id -g) docker-compose exec -T db mysql -u root --password="PASSWORD" taxonomy < ./taxonomy.sql
+CURRENT_UID=$(id -u):$(id -g) docker-compose exec -T db mysql -u root --password="PASSWORD" taxonomy < ./taxonomy_to_import.sql
 
 echo "Installation complete."
 
