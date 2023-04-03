3a7414f2ad05e26de09a3466758a3c2737ba1ba8
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2023-04-03 08:23:43 +0000

    manage visualization of the list of taxonomies during the search.

--- html_new/components/com_finder/tmpl/search/default.php
+++ html_new/components/com_finder/tmpl/search/default.php
@@ -29,26 +29,21 @@ $db->setQuery($query);
 $results = $db->loadObjectList();
 
 if (isset($_GET['cat']))
-        $namee = $_GET['cat'];
+   $namee = $_GET['cat'];
 else
-        $namee = 'a';
-// echo $namee;
+   $namee = 'a';
 
 // Create a new query object.
 $sql_term = $db->getQuery(true);
 
 $sql_term->select('*')
       ->from($db->quoteName('taxonomy_content'))
-      // ->where($db->quoteName('title') . $namee)
       ->where($db->quoteName('title') . ' like ' . $db->quote($namee . '%'))
       ->where($db->quoteName('catid') . ' = ' . $db->quote('8'))
       ->where($db->quoteName('state') . ' = ' . $db->quote('1'))
       ->order($db->quoteName('title') . ' ASC'); 
-//$sql_term = sprintf("SELECT * FROM `taxonomy_content` where title like '%s%%' and catid = '8' and state = '1' ORDER BY title ASC ",
-//$db->escape($namee));
 $db->setQuery($sql_term);
 $results_term = $db->loadObjectList();
-echo $result_term; 
 ?>
 <div class="com-finder finder">
     <?php if ($this->params->get('show_page_heading')) : ?>
@@ -72,41 +67,18 @@ echo $result_term;
 </div>
 
 <!-- GEM custom -->
-<!--<div class="term-let">
-        <?php
-          # foreach($results as $rows){
-          #   //control if exist terms with specific cat
-          #   $cat = $rows->cat;
-          #   $sql_term_id = sprintf("SELECT * FROM `taxonomy_content` where title like '%s%%' and catid = '8' and state = '1' ",
-          #                          $db->escape($cat));
-          #   $db->setQuery($sql_term_id);
-          #   $count_results_term = $db->loadObjectList();
-          #   $db->query($sql_term_id);
-          #   $count_results = $db->getNumRows();
-          #   if($count_results != 0){
-          #       echo'
-          #       <a class="let-cat m'.$cat.'" href="'.$this->baseurl.'/?cat='.$cat.'">
-          #               <div class="div-let-cat">
-          #                       '.ucfirst($cat).'
-          #               </div>
-          #       </a>';
-          #   }
-          # }
-        ?>
-    </div>
-        <div style="clear:both;"></div>-->
-
-        <?php
-              if (!empty($results_term)) {
-                foreach($results_term as $rows){
-        ?>
-                <div class="col-md-4">
-                  <h2><a class="tit-term" style="" href="terms/<?php echo $rows->alias; ?>"><span class="icon-arrow-right"></span> <?php echo ucfirst($rows->title); ?></a></h2>
-                </div>
-        <?php
-                }
-              }else{
-                echo '<span style="font-weight: 700;">No term this</span>';
-              }
-        ?>
-
+<?php if( !isset($_GET['q']) ) { ?>
+<?php
+      if (!empty($results_term)) {
+        foreach($results_term as $rows){
+?>
+        <div class="col-md-4">
+          <h2><a class="tit-term" style="" href="terms/<?php echo $rows->alias; ?>"><span class="icon-arrow-right"></span> <?php echo ucfirst($rows->title); ?></a></h2>
+        </div>
+<?php
+        }
+      }else{
+        echo '<span style="font-weight: 700;">No term this</span>';
+      }
+?>
+<?php } ?>
