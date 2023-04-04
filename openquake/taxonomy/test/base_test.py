07f871d31d162e29eb55efd84d2195d64c40ee99
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2023-04-04 12:52:24 +0000

    refactoring administrator test

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -26,10 +26,6 @@ class TaxonomyAllTest(unittest.TestCase):
                                    'calculate', timeout=10)
 
     def search_test(self):
-        # pla.get(
-        #         'index.php/component/search/'
-        #         '?searchword=&searchphrase=all&Itemid=101'
-        #         )
 
         varsearch = 'Assembly [ASS]'
 
@@ -45,8 +41,6 @@ class TaxonomyAllTest(unittest.TestCase):
             "//a[@href='/terms/assembly-ass?highlight=WyJhc3NlbWJseSIsImFzcyJd']")
         search_term.click()
 
-        # pla.wait_new_page(search_term, 'terms/assembly-ass?highlight'
-        #                                '=WyJhc3NlbWJseSIsImFzcyJd', timeout=5)
 
     def content_test(self):
 
@@ -98,14 +92,6 @@ class TaxonomyAllTest(unittest.TestCase):
         pla.wait_new_page(mono_alias_termlink, 'terms/floor', timeout=5)
 
 
-def new_term():
-    pla.get('')
-
-    new_term = pla.xpath_finduniq(
-        "//a[normalize-space(text())='New definition']")
-    new_term.click()
-
-
 class TaxonomyInOutTest(unittest.TestCase):
 
     @classmethod
@@ -135,9 +121,6 @@ class TaxonomyInOutTest(unittest.TestCase):
             "//button[@type='submit']")
         submit_login.click()
 
-        # pla.wait_new_page(submit_login, 'index.php/component/users/'
-        #                                 '?view=login&Itemid=101', timeout=10)
-        #                                 # 'profile?Itemid=101', timeout=10)
 
     @classmethod
     def tearDownClass(cls):
@@ -171,31 +154,6 @@ class TaxonomyInOutTest(unittest.TestCase):
             " @name='jform[title]']")
         insert_title_field.send_keys(exex)
 
-    def alias_test(self):
-        # check input Alias
-        new_term()
-        alias = pla.xpath_find_base(
-            "//input[@id='jform_alias'"
-            " and @placeholder='Auto-generate from title']")
-        if len(alias) > 0:
-            raise ValueError('Tag input Alias must be not present')
-
-    def tab_metadata_test(self):
-        # check tab Metadata
-        new_term()
-        metadata = pla.xpath_find_base(
-            "//a[normalize-space(text())='Metadata']")
-        if len(metadata) > 0:
-            raise ValueError('Tab Metadata must be not present')
-
-    def tab_publishing_test(self):
-        # check tab Publishing
-        new_term()
-        language = pla.xpath_find_base(
-            "//a[normalize-space(text())='Language']")
-        if len(language) > 0:
-            raise ValueError('Tab Language must be not present')
-
 
 class TaxonomyAdminTest(unittest.TestCase):
 
@@ -206,29 +164,11 @@ class TaxonomyAdminTest(unittest.TestCase):
         admin_field = pla.xpath_finduniq(
             "//input[@id='mod-login-username' and @type='text' and"
             " @name='username']")
-        admin_field.send_keys(pla.user)
 
         admin_field = pla.xpath_finduniq(
             "//input[@id='mod-login-password' and @type='password' and"
             " @name='passwd']")
-        admin_field.send_keys(pla.passwd)
 
         submit_login_admin = pla.xpath_finduniq(
             "//button[@class='btn btn-primary btn-block btn-large"
             " login-button']")
-        submit_login_admin.click()
-
-        # go to list articles from menu
-        cont_menu = pla.xpath_finduniq(
-            "//a[@class='dropdown-toggle'"
-            " and normalize-space(text())='Content']")
-        cont_menu.click()
-
-        art_menu = pla.xpath_finduniq(
-            "//a[@class='dropdown-toggle menu-article'"
-            " and normalize-space(text())='Articles']")
-        art_menu.click()
-
-        pla.xpath_findfirst(
-            "//a[@class='hasTooltip'"
-            " and normalize-space(text())='Infilled Frame']")
