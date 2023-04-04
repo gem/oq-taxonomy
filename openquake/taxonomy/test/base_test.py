bbd0fa0b3de866faef5a8ee7a35284fbadd03e16
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2023-04-04 09:43:46 +0000

    added complete path in pla_wait_page function for search test

--- openquake/taxonomy/test/base_test.py
+++ openquake/taxonomy/test/base_test.py
@@ -45,7 +45,8 @@ class TaxonomyAllTest(unittest.TestCase):
             "//a[@href='/terms/assembly-ass?highlight=WyJhc3NlbWJseSIsImFzcyJd']")
         search_term.click()
 
-        pla.wait_new_page(search_term, 'terms/assembly-ass', timeout=5)
+        pla.wait_new_page(search_term, '/terms/assembly-ass?highlight'
+                                       '=WyJhc3NlbWJseSIsImFzcyJd', timeout=5)
 
     def content_test(self):
 
