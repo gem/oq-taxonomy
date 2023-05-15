4210bc8b36e24cfa61a9b5bb09da5ffa8402a69b
hascar <armando.scarpati@globalquakemodel.org>
2023-05-15 10:42:19 +0200

    added copy all files

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -58,8 +58,8 @@ rm -rf $HOME/$GEM_GIT_PACKAGE/site/images/headers
 # cp $HOME/$GEM_GIT_PACKAGE/configuration.php.tmpl $HOME/$GEM_GIT_PACKAGE/site/configuration.php
 
 # copy folder $GEM_GIT_PACKAGE from home lxc to /var/www/html
-cp -R $HOME/$GEM_GIT_PACKAGE/html/configuration.php $HOME/$GEM_GIT_PACKAGE/html/.htaccess $HOME/$GEM_GIT_PACKAGE/site
-# cp -R $HOME/$GEM_GIT_PACKAGE/html/* $HOME/$GEM_GIT_PACKAGE/html/.htaccess $HOME/$GEM_GIT_PACKAGE/site
+# cp -R $HOME/$GEM_GIT_PACKAGE/html/configuration.php $HOME/$GEM_GIT_PACKAGE/html/.htaccess $HOME/$GEM_GIT_PACKAGE/site
+cp -R $HOME/$GEM_GIT_PACKAGE/html/* $HOME/$GEM_GIT_PACKAGE/html/.htaccess $HOME/$GEM_GIT_PACKAGE/site
 
 sleep 70
  
