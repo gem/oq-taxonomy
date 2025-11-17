856ec27fe3d710dae03e7558a01b360f02165149
hascar <armando.scarpati@globalquakemodel.org>
2025-11-17 14:27:21 +0100

    deleted inst_docker function from verifier script

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -17,24 +17,7 @@ sudo apt-get -y install git ca-certificates wget
 cd $GEM_GIT_PACKAGE
 cp dev-env-sample .env 
 
-inst_docker () {
-    # install requirements for docker
-    sudo apt-get -y install apt-transport-https ca-certificates curl \
-         gnupg lsb-release
-    # install docker-ce and docker-compose
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get --fix-missing update 
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-    # install stable release of docker-compose
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-}
-
 #installation of docker and docker-compose
-inst_docker
 id
 
 #power on of docker database
