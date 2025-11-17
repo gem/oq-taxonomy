283585cd5ee5724a1fc98430b2f049bb939676c0
hascar <armando.scarpati@globalquakemodel.org>
2025-11-17 17:02:14 +0100

    deleted pip==20.3 installation

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -71,7 +71,6 @@ echo "Installation complete."
 #function complete procedure for tests
 exec_test () {    
     #install selenium,pip,geckodriver,clone oq-moon and execute tests with nose 
-    sudo python3.12 -m pip install --default-timeout=100 --upgrade pip==20.3
     sudo python3.12 -m pip install setuptools==69.0.2
     sudo python3.12 -m pip install nose
     wget "https://ftp.openquake.org/common/selenium-deps"
